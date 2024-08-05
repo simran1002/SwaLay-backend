22f7eddd3beec1974a71ffd9573da6271bdad92d
Simran_Yadav <simranyadav464@gmail.com>
2024-08-06 01:27:51 +0530

    Added the update status youtube

--- src/app/api/youtube/updateStatus/route.ts
+++ src/app/api/youtube/updateStatus/route.ts
@@ -1,22 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { connect } from '@/dbConfig/dbConfig';
-import mongoose from 'mongoose';
-import Youtube from '../../../../models/youtube'; // adjust the path as necessary
-import { response } from '@/lib/response'; // Adjust the import path as necessary
+import Youtube from '../../../../models/youtube'; 
+import { response } from '@/lib/response';
 
 export async function PUT(req: NextRequest) {
   try {
     await connect();
 
-    // Extract the id from the query parameters
     const id = req.nextUrl.searchParams.get('id');
-    if (!id) {
-      return response(400, null, false, 'ID is required').nextResponse;
+    if (!id || isNaN(Number(id))) {
+      return response(400, null, false, 'Valid ID is required').nextResponse;
     }
 
     // Parse the request body
     const body = await req.json();
     const { status } = body;
+
+    if (typeof status !== 'boolean') {
+      return response(400, null, false, 'Valid status is required').nextResponse;
+    }
 
     // Update the Youtube entry status
     const updatedYoutube = await Youtube.findOneAndUpdate(
@@ -26,14 +28,12 @@ export async function PUT(req: NextRequest) {
     );
 
     if (!updatedYoutube) {
-      return response(404, null, false, 'Youtube copyright entry not found').nextResponse;
+      return response(404, null, false, 'YouTube entry not found').nextResponse;
     }
 
-    return response(200, updatedYoutube, true, 'Youtube copyright entry status updated').nextResponse;
+    return response(200, updatedYoutube, true, 'YouTube entry status updated').nextResponse;
   } catch (error) {
     console.error('Internal server error:', error);
     return response(500, error, false, 'Internal server error').nextResponse;
-  } finally {
-    mongoose.connection.close();
   }
 }
