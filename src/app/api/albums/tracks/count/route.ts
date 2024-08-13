04082bf59973c26f475ab3a43c368053dcb115c3
Simran_Yadav <simranyadav464@gmail.com>
2024-08-13 08:48:34 +0530

    did some minor fixing

--- src/app/api/albums/tracks/count/route.ts
+++ src/app/api/albums/tracks/count/route.ts
@@ -1,23 +1,17 @@
-import Album from '@/models/albums'; // Ensure this path is correct
-import Track from '@/models/track'; // Ensure this path is correct
-import { connect } from '@/dbConfig/dbConfig'; // Ensure this path is correct
-import response from '@/lib/response'; // Adjust the path according to your file structure
+import Album from '@/models/albums'; 
+import Track from '@/models/track';
+import { connect } from '@/dbConfig/dbConfig'; 
+import response from '@/lib/response';
 
 export async function GET() {
   try {
-    // Connect to the database
     await connect();
-
-    // Get the total number of albums
     const totalAlbums = await Album.countDocuments();
 
-    // Get the total number of tracks
     const totalTracks = await Track.countDocuments();
 
-    // Respond with the totals using the response function
     return response(200, { totalAlbums, totalTracks }, true, 'Data fetched successfully').nextResponse;
   } catch (error) {
-    // Handle errors using the response function
     console.error(error);
     return response(500, null, false, 'Internal Server Error').nextResponse;
   }
