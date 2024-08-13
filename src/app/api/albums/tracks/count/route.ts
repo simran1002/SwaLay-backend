1405f883395103c43380adc4a30b1f63cbe18d30
Simran_Yadav <simranyadav464@gmail.com>
2024-08-13 08:54:20 +0530

    Added the unique artist count

--- src/app/api/albums/tracks/count/route.ts
+++ src/app/api/albums/tracks/count/route.ts
@@ -6,11 +6,12 @@ import response from '@/lib/response';
 export async function GET() {
   try {
     await connect();
+
     const totalAlbums = await Album.countDocuments();
-
     const totalTracks = await Track.countDocuments();
+    const uniqueArtists = await Album.distinct('artist').then(artists => artists.length);
 
-    return response(200, { totalAlbums, totalTracks }, true, 'Data fetched successfully').nextResponse;
+    return response(200, { totalAlbums, totalTracks, uniqueArtists }, true, 'Data fetched successfully').nextResponse;
   } catch (error) {
     console.error(error);
     return response(500, null, false, 'Internal Server Error').nextResponse;
